696b7b69a3fa64ed1206ff346024cda0df627a93
Mugisha Kelly <mugishakelly270@gmail.com>
2026-02-12 05:13:49 -0800

    Add products array with sample product data

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: "Smart Watch",
+    price: 50000,
+    image: "products/watch.jpg"
+  },
+  {
+    id: 2,
+    name: "Wireless Headphones",
+    price: 35000,
+    image: "products/headphone.jpg"
+  },
+  {
+    id: 3,
+    name: "Laptop Backpack",
+    price: 25000,
+    image: "products/bag.jpg"
+  }
+];
